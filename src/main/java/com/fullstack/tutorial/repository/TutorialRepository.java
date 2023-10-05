af8d83aef8d1fa64dbd1e6cd8fb8bf7a9fab6122
sakshijain20 <jain.sakshi1099@gmail.com>
2023-10-06 02:08:43 +0530

    init commit

---
+++ src/main/java/com/fullstack/tutorial/repository/TutorialRepository.java
@@ -0,0 +1,16 @@
+package com.fullstack.tutorial.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fullstack.tutorial.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long>{
+
+	  List<Tutorial> findByPublished(boolean published);
+	  List<Tutorial> findByTitleContaining(String title);
+	
+
+}
